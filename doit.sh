38e7dfa3fa9617676d819831581472cd7df8cd89
heku66 <102395422+heku66@users.noreply.github.com>
2022-06-17 17:51:47 +0800

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -39,9 +39,12 @@ opsys() {
   fi
 }
 
+
 version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
+
 opsys
-wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-s390x
+
+wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-$MACHINE
 
 chmod 755 /etc/hysteria/hysteria
 cd /etc/hysteria
