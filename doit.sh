158e36ca5b6e4759e064f812475b23f268e3f870
heku66 <102395422+heku66@users.noreply.github.com>
2022-06-17 08:29:48 +0800

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -7,7 +7,7 @@ version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/
 wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-amd64
 
 chmod 755 /etc/hysteria/hysteria
-cd /etc/hysteria/hysteria
+cd /etc/hysteria
 ip=`curl -4 -s ip.sb`
 openssl ecparam -genkey -name prime256v1 -out ca.key
 openssl req -new -x509 -days 36500 -key ca.key -out ca.crt  -subj "/CN=bing.com"
