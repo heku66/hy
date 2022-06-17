0167a3b7ac82f64b0f9961d87314836153f57db8
heku66 <102395422+heku66@users.noreply.github.com>
2022-06-17 08:58:08 +0800

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 mkdir -p /etc/hysteria
 version=`wget -qO- -t1 -T2 --no-check-certificate "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
 
-wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-amd64
+wget -q -O /etc/hysteria/hysteria --no-check-certificate https://github.com/HyNetwork/hysteria/releases/download/$version/hysteria-linux-s390x
 
 chmod 755 /etc/hysteria/hysteria
 cd /etc/hysteria
